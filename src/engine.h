6f93ad4bd80bbb839ee9f607a7483e4e93bfe5d7
AliEn707 <1alien707@qip.ru>
2015-12-14 13:07:38 +0300

    added endianess swap macro

--- src/engine.h
+++ src/engine.h
@@ -17,3 +17,25 @@ void cleanAll();
 /*
 void syncTime(struct timeval t, unsigned int time);
 */
+
+
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+
+#define biteSwap(a) ({ typeof(a) val=a;\
+	if (sizeof(a)==2){\
+		val=(val << 8) | ((val >> 8) & 0xFF);\
+	}else if (sizeof(a)==4){\
+		val = ((val << 8) & 0xFF00FF00) | ((val >> 8) & 0xFF00FF );\
+		val = (val << 16) | ((val >> 16) & 0xFFFF);\
+	} else if (sizeof(a) == 8){\
+		val = ((val << 8) & 0xFF00FF00FF00FF00ULL ) | ((val >> 8) & 0x00FF00FF00FF00FFULL );\
+		val = ((val << 16) & 0xFFFF0000FFFF0000ULL ) | ((val >> 16) & 0x0000FFFF0000FFFFULL );\
+		val = (val << 32) | ((val >> 32) & 0xFFFFFFFFULL);\
+	}\
+val;})
+
+#else
+
+#define biteSwap(a) a
+
+#endif
